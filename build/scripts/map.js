5fc904920381abd22f07414ec60e431672db5b8b
Anna <anna@192.168.1.4>
2017-05-12 00:43:43 +0300

    add interactive map

--- build/scripts/map.js
+++ build/scripts/map.js
@@ -1,10 +1,54 @@
 var regions = document.querySelectorAll('.mapst0');
+var points = document.querySelectorAll('.mapst1');
+var createDom = document.getElementById('create-value');
+var populationDom = document.getElementById('population-value');
+var workingDom = document.getElementById('working-value');
+var imageDom = document.getElementById('image-value');
+var titleDom = document.getElementById('title-value');
 
 for (let i = 0; i < regions.length; i++) {
 	regions[i].onclick = function() {
-		for(let j = 0; j < regions.length; j++) {
-			regions[j].classList.remove('active');
+		removeActiveClass(regions, this);
+		redirectTo(regionsData, this);
+	}
+	regions[i].onmouseover = function() {
+		setInfo(regionsData, this);
+	}
+}
+
+for (let i = 0; i < points.length; i++) {
+	points[i].onclick = function() {
+		removeActiveClass(points, this);
+		redirectTo(pointsData, this);
+	}
+	points[i].onmouseover = function() {
+		setInfo(pointsData, this);
+	}
+}
+
+function removeActiveClass(array, item) {
+	for(let i = 0; i < array.length; i++) {
+		array[i].classList.remove('active');
+	}
+	item.classList.add('active');
+}
+
+function setInfo(array, item) {
+	for(let i = 0; i < array.length; i++) {
+		if (array[i].id === item.id) {
+			imageDom.src = array[i].img;
+			titleDom.textContent = array[i].name;
+			createDom.textContent = array[i].est;
+			populationDom.textContent = array[i].population;
+			workingDom.textContent = array[i].working_places;
 		}
-		this.classList.add('active');
+	}
+}
+
+function redirectTo(array, item) {
+	for(let i = 0; i < array.length; i++) {
+		if (array[i].id === item.id) {
+			window.location.href = array[i].link;
+		}
 	}
 }
---
+++ src/scripts/data.js
@@ -0,0 +1,482 @@
+var regionsData = [
+	{
+		id: 'region-1',
+		name: 'Івано-Франківська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-2',
+		name: 'Закарпатська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-3',
+		name: 'Тернопільська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-4',
+		name: 'Волинська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-5',
+		name: 'Львівська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-6',
+		name: 'Черкаська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-7',
+		name: 'Рівненська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-8',
+		name: 'Чернівецька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-9',
+		name: 'Одеська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-10',
+		name: 'Хмельницька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-11',
+		name: 'Вінницька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-12',
+		name: 'Житомирська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-13',
+		name: 'Чернігівська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-14',
+		name: 'Київська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-15',
+		name: 'Кіровоградська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-16',
+		name: 'ОТГ АР Крим',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-17',
+		name: 'Херсонська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-18',
+		name: 'Миколаївська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-19',
+		name: 'Сумська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-20',
+		name: 'Луганська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-21',
+		name: 'Запорізька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-22',
+		name: 'Харківська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-23',
+		name: 'Полтавська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-24',
+		name: 'Дніпропетровська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-25',
+		name: 'Донецька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'region-26',
+		name: 'ОТГ м.Київ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	}
+];
+
+var pointsData = [
+	{
+		id: 'point-1',
+		name: 'Івано-Франківська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-2',
+		name: 'Закарпатська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-3',
+		name: 'Тернопільська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-4',
+		name: 'Волинська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-5',
+		name: 'Львівська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-6',
+		name: 'Черкаська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-7',
+		name: 'Рівненська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-8',
+		name: 'Чернівецька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-9',
+		name: 'Одеська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-10',
+		name: 'Хмельницька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-11',
+		name: 'Вінницька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-12',
+		name: 'Житомирська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-13',
+		name: 'Чернігівська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-14',
+		name: 'Київська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-15',
+		name: 'Кіровоградська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-16',
+		name: 'ОТГ АР Крим',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-17',
+		name: 'Херсонська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-18',
+		name: 'Миколаївська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-19',
+		name: 'Сумська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-20',
+		name: 'Луганська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-21',
+		name: 'Запорізька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-22',
+		name: 'Харківська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-23',
+		name: 'Полтавська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-24',
+		name: 'Дніпропетровська ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-25',
+		name: 'Донецька ОТГ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-26',
+		name: 'ОТГ м.Київ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	},
+	{
+		id: 'point-27',
+		name: 'ОТГ м.Київ',
+		link: 'www.facebook.com',
+		est: 'Створено ОТГ',
+		population: '456',
+		working_places: '234',
+		img: 'images/ha.jpg'
+	}
+];
