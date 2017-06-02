aaba23d306bf7155a97329f55f4516a25d9f033a
Anna <anna.bratseiko@gmail.com>
2017-06-02 07:55:57 +0300

    add adaptive

--- build/scripts/animation.js
+++ build/scripts/animation.js
@@ -1,2 +1,11 @@
 window.sr = ScrollReveal();
 sr.reveal('.animate', { duration: 700 });
+
+var mobileMenu = document.querySelector('.mobile-menu-popup');	
+document.querySelector('.header-menu-link').addEventListener('click', function() {
+	mobileMenu.classList.add('open');
+});
+
+document.querySelector('.mobile-menu-popup-close').addEventListener('click', function() {
+	mobileMenu.classList.remove('open');
+});
